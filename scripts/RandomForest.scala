e0d282122451d5b6c669a01ca27d4e6b69697c4e
Ji Su Lee <32809393+LogSigma@users.noreply.github.com>
2018-02-27 08:55:04 +0900

    Update RandomForest.scala

--- scripts/RandomForest.scala
+++ scripts/RandomForest.scala
@@ -1,5 +1,19 @@
 class RandomForestAnalysis extends Serializable with ModelService{
   def evaluate(sc: SparkContext, df: DataFrame, args: Array[(Object)]): DataFrame = {
   
+    val sqlContext - new SQLContet(sc)
+    import sqlContext.implicits._
+    val configuration - new Configuration()
+    
+    configuration.set("hbase.zookeeper.quorum",args(0).toString())
+    configuration.set("hbase.zookeeper.property.clientPort","2181")
+    
+    val jsonString = args(3).toString()
+    
+    val lot = sc.parallelize(jsonString :: Nil)
+    val schema = (new StructType).add("ALIAS_LOT_WF_ID",StringType).add("LABEL",StringType)
+    val lotdf = sqlContext.read.schema(schema).json(lot)
+    
+    
   }
 }
